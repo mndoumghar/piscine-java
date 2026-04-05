1cdc173d30e17ece7565fd9b85ab5f1d14d975b2
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-05 22:18:07 +0100

    Add StringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return "";
+        }
         return s1 + s2;
     }
 }
