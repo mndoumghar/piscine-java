3197a607a784ccfbc764ddfe2dd309c9c04e5937
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-05 20:38:19 +0100

    Add enum Chiformi

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,16 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        
+       switch (chifoumiAction) {
+            case ROCK: 
+                return  ChifoumiAction.SCISSOR;     
+            case PAPER:
+                return  ChifoumiAction.ROCK;
+            case SCISSOR :
+                return  ChifoumiAction.PAPER;
+            default:
+                break;
+        }
+         return  ChifoumiAction.PAPER;
+    }
+}
