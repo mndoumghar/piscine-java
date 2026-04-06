4364c5558161c0bf0763e29f4452d2b95fc20150
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-07 00:14:12 +0100

    Add Cat

---
+++ CatInFile/CatInFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class CatInFile {
+    public static void cat(String[] args) throws IOException {
+        if (args.length == 0) {
+            return;
+        }
+
+        FileInputStream reader = new FileInputStream(args[0]);
+        byte[] buffer = new byte[1000];
+        int x;
+        while ((x = reader.read(buffer)) != -1) {
+            System.out.write(buffer,0,x);
+        }
+        reader.close();
+    }
+}
