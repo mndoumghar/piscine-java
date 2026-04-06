02e522e174073d5de848352598ab2eefa7edba7a
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-06 23:47:15 +0100

    Add Cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+
 public class Cat {
     public static void cat(String[] args) throws IOException {
         if (args.length == 0) {
             return;
         }
-        for (String filename : args) {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            reader.close();
+
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.print(line);
         }
+        reader.close();
 
     }
 }
