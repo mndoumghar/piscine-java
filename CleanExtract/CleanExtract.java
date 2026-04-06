ee65cf717baebbca1d3fda744ff5902838365f03
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-06 06:02:33 +0100

    CleanExtract ..

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -9,12 +9,11 @@ public class CleanExtract {
 
             String  res = word.trim();
             if (res.startsWith(".") && res.endsWith(".")) {
-                System.out.println();
                 result += res.substring(1, res.length() -1)+ " ";
             } else if (res.contains(".")) {
                 int findpoint = res.indexOf(".");
                 if (findpoint>= 0) {
-                    if (res.indexOf(".")+1  == res.lastIndexOf(".")) {
+                    if (findpoint+1  == res.lastIndexOf(".")) {
                         continue;
                     }
                     result += res.substring(findpoint+1, res.length())+ " " ;
