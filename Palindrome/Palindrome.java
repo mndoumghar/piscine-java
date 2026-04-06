8bd2ae786581af9460872d40ef93527164af8b53
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-06 06:22:38 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,6 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s == null) {return false;}
         char[] word = s.toLowerCase().toCharArray();
 
         for (int i = 0; i < s.length() ; i++) {
