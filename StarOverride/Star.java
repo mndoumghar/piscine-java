195e6212eb5740923868539f66265b839e900205
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 14:32:57 +0100

    Override concept

--- StarOverride/Star.java
+++ StarOverride/Star.java
@@ -17,21 +17,17 @@ public class Star extends CelestialObject {
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        if (o == null || getClass() != o.getClass())
+        if (!(o instanceof Star))
             return false;
-
+        if (!super.equals(o))
+            return false;
         Star star = (Star) o;
-
-        return Double.compare(star.magnitude, magnitude) == 0
-                && Objects.equals(getName(), star.getName())
-                && Double.compare(getX(), star.getX()) == 0
-                && Double.compare(getY(), star.getY()) == 0
-                && Double.compare(getZ(), star.getZ()) == 0;
+        return Double.compare(star.magnitude, magnitude) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(), getX(), getY(), getZ(),magnitude);
+        return Objects.hash(getName(), getX(), getY(), getZ(), magnitude);
     }
 
     public double getMagnitude() {
