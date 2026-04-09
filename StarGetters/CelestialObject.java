b3c138afde6a5f508559cfad504e3e1ab8b8141b
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 10:09:54 +0100

    Getter && Setter

--- StarGetters/CelestialObject.java
+++ StarGetters/CelestialObject.java
@@ -1,43 +1,51 @@
 public class CelestialObject {
-    private double x;
-    private double y;
-    private double z;
-    private String name;
-    private CelestialObject(String name,double x,double y,double z) {
-        this.name =name;
-        this.x= x;
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
         this.y = y;
         this.z = z;
     }
+
     public CelestialObject() {
         this.name = "Soleil";
-        
+
     }
 
     public void setX(double x) {
         this.x = x;
     }
-     public void setY(double y) {
+
+    public void setY(double y) {
         this.y = y;
     }
-     public void setZ(double z) {
+
+    public void setZ(double z) {
         this.z = z;
     }
-     public void setName(String name) {
+
+    public void setName(String name) {
         this.name = name;
     }
 
     public double getX() {
         return x;
     }
-     public double getY() {
+
+    public double getY() {
         return y;
     }
-     public double getZ() {
+
+    public double getZ() {
         return z;
     }
-    public String  getName() {
+
+    public String getName() {
         return name;
     }
-    
+
 }
