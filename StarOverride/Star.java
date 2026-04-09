08401f11383b71b147aa7aac36d60d48d1d329d9
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 13:49:35 +0100

    starOverride

---
+++ StarOverride/Star.java
@@ -0,0 +1,27 @@
+
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star() {
+        super();
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+
+    }
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+     public String toString() {
+        return String.format("%s shines at the %.3f magnitude",getName(), getMagnitude());
+    }
+    
+   
+}
