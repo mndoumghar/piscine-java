0fbc172cdac4741b0fc63e6898d8f4b01126d2ff
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 14:19:33 +0100

    Override concept

--- StarOverride/Star.java
+++ StarOverride/Star.java
@@ -22,7 +22,7 @@ public class Star extends CelestialObject {
         }
     @Override
     public int hashCode() {
-        return Objects.hash(magnitude);
+        return Objects.hash(getName(),getX(),getY(),getZ(),magnitude);
     }
 
     public double getMagnitude() {
