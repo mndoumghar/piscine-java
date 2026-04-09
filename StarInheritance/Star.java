2b435db632ee0f26bb0f73c5dc3ec37aae7ef3dc
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 13:33:13 +0100

    les retage <extends>

---
+++ StarInheritance/Star.java
@@ -0,0 +1,15 @@
+
+
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star() {
+        super();
+    }
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+}
