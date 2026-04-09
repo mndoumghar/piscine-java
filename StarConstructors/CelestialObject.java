5989146a0840132c63b8fb4e8c2e06edcf0debae
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-09 09:58:21 +0100

    Add Constructrue

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,16 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    public CelestialObject(String name,double x,double y,double z) {
+        this.name =name;
+        this.x= x;
+        this.y = y;
+        this.z = z;
+    }
+    public CelestialObject() {
+        
+    }
+    
+}
