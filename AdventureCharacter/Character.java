06b5faf9a5e5d6d223367b889228419aeed272d2
mndoumghar <mohamedndoumghar@gmail.com>
2026-04-13 16:13:56 +0100

    AdventureCharacter

--- AdventureCharacter/Character.java
+++ AdventureCharacter/Character.java
@@ -27,13 +27,27 @@ public class Character {
         return getName()+" : "+getCuurent()+ "/" +getMAx();
     }
     public void takeDamage(int amount) {
-        this.currentHealth =   getCuurent() -amount ;
+        this.currentHealth -= amount ;
 
     } 
     public void attack(Character obj) {
         obj.takeDamage(9);
     }
-     
+       public static void main(String[] args) {
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 5);
+        
+        System.out.println(aragorn.toString());
+        System.out.println(uruk.toString());
+        
+        aragorn.attack(uruk);
+
+        System.out.println(uruk.toString());
+        
+        aragorn.takeDamage(12);
+
+        System.out.println(aragorn.toString());
+    }
 
 
 }
